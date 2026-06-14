d47b1f043e9433634500584415b528fc2615bbda
Michael Ferreira <michaeljf.2007@gmail.com>
2026-06-14 17:07:22 -0400

    feat: blog

---
+++ app/blog/page.tsx
@@ -0,0 +1,12 @@
+import { SectionHeading } from "../components/SectionHeading";
+
+export default function Blog() {
+    return (
+        <section id="blog">
+            <SectionHeading>Blog</SectionHeading>
+            <div className="space-y-8">
+                <p>Coming soon...</p>
+            </div>
+        </section>
+    );
+}

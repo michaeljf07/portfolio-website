e5880932df53d8506a767de90fb0cf6ce62ce498
Michael Ferreira <michaeljf.2007@gmail.com>
2026-06-21 15:36:54 -0400

    added first blog posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,21 @@
 import { SectionHeading } from "../components/SectionHeading";
+import { getAllPosts } from "@/app/lib/markdown";
+import BlogCard from "@/app/components/blog/BlogCard";
 
 export default function Blog() {
     return (
         <section id="blog">
             <SectionHeading>Blog</SectionHeading>
-            <div className="space-y-8">
-                <p>Coming soon...</p>
+            <div>
+                {getAllPosts().map((post) => (
+                    <BlogCard
+                        key={post.slug}
+                        title={post.title}
+                        excerpt={post.excerpt}
+                        slug={post.slug}
+                        date={post.date}
+                    />
+                ))}
             </div>
         </section>
     );

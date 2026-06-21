bcf28ba569f22d616d9960aceb58b126079463b5
Michael Ferreira <michaeljf.2007@gmail.com>
2026-06-21 15:43:51 -0400

    fix: add back button to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,17 @@
 import { SectionHeading } from "../components/SectionHeading";
 import { getAllPosts } from "@/app/lib/markdown";
 import BlogCard from "@/app/components/blog/BlogCard";
+import Link from "next/link";
 
 export default function Blog() {
     return (
         <section id="blog">
+            <Link
+                href="/"
+                className="text-sm text-text-muted hover:text-text no-underline hover:no-underline mb-6 inline-block"
+            >
+                ← Back to home
+            </Link>
             <SectionHeading>Blog</SectionHeading>
             <div>
                 {getAllPosts().map((post) => (
